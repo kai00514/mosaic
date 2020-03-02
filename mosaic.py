ba60cd9ac18f4dcfd2ae9e1de92bec2130e58a56
k.fuji <k.fujishima@edge-consulting.work>
2020-03-02 17:23:09 +0900

    fixed

--- mosaic.py
+++ mosaic.py
@@ -2,22 +2,21 @@ import cv2
 import sys
 import os
 import glob
-import numpy
 
 
 cascade_path = "/home/fujishima0514/fujishima/.local/lib/python3.5/site-packages/cv2/data/haarcascade_frontalface_default.xml"
 
-#def usage(arg):
+def usage():
+    print(sys.argv[0] + "<input dir> <output_dir>")
 
 
-def topical_mosaic(img,rect,size):  
+def topical_mosaic(img,rect):  
 
     # topical 関数を呼ぶ
     # i_rect を返す
     tpcl = topical(rect,img)
      
     tpcl_mos = mosaic(tpcl)
-    #tpcl_mos = mosaic(tpcl,size)
     
 
     (x1,y1,x2,y2)=rect  
@@ -62,20 +61,19 @@ if __name__ =="__main__":
    
             
     if len(arg) < 3: 
-        usage(arg)
+        usage()
         quit(-1)
     elif len(arg) == 3:
         input_dir = arg[1]
         output_dir = arg[2]
     if not os.path.exists(output_dir):
-        output_img_dir = os.mkdir(output_dir)
+        os.mkdir(output_dir)
     else:
         print("Allready exist ")
 
-    
-    if os.path.isfile(input_dir) == True:
-        usage(arg)
-        quit(-2)
+#    if os.path.isfile(input_dir) == True:
+#        usage(arg)
+#        quit(-2)
 
 
 
@@ -88,9 +86,12 @@ if __name__ =="__main__":
         #print(image.shape)
 		#color = (0, 0, 255)
         basename = os.path.basename(fname)
-        #output = os.path.join(output_img_dir ,basename)
-        output = os.path.join("./out",basename)
-        output2 = os.path.join("./output",basename)
+        output = os.path.join(output_dir ,basename)
+        print(output)
+        glob_output = glob.glob(output)
+#        print(output)
+      #  output = os.path.join("./out",basename)
+      #  output2 = os.path.join("./output",basename)
         output3 = os.path.join("./gray_out",basename)
 
         #for (x,y,w,h) in facerect:
@@ -105,24 +106,36 @@ if __name__ =="__main__":
            #gray_mosaic = image2gray(gray)
             tpcl_image = topical(rect,image)
             gray_image = image2gray(tpcl_image)
-            mosaic_gray = mosaic(gray_image)
+            mosaic_image = mosaic(gray_image)
+#def topical_mosaic(img,rect,size):  
+            #tpcl_mosaic = topical_mosaic(image, rect)
+
 
             (x1,y1,x2,y2)=rect  
             img2=image.copy()   # normal image
-            img2[y1:y2,x1:x2,0]=mosaic_gray # only mosai
-            img2[y1:y2,x1:x2,1]=mosaic_gray # only mosai
-            img2[y1:y2,x1:x2,2]=mosaic_gray # only mosai
+            #img2[y1:y2,x1:x2,0]=mosaic_gray # only mosai
+            #img2[y1:y2,x1:x2,1]=mosaic_gray # only mosai
+            #img2[y1:y2,x1:x2,2]=mosaic_gray # only mosai
+
+#debug####
+            print("debug shape")
+            print(img2[y1:y2,x1:x2,0].shape, mosaic_image.shape) 
+
+            img2[y1:y2,x1:x2,0]= mosaic_image # only mosai
+            img2[y1:y2,x1:x2,1]= mosaic_image # only mosai
+            img2[y1:y2,x1:x2,2]= mosaic_image # only mosai
             
             color = (0, 0, 255)
 
             #cv2.rectangle(img, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
            # cv2.rectangle(gray_mosaic, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
             print("complete")
-            print("outputted in " + output)
             print("outputted in " + output3)
       #  cv2.imwrite(output, img)
         #cv2.imwrite(output3,gray_mosaic)
-            pv2.imwrite(output3,img2)
+            #cv2.imwrite(output_img_dir,img2)
+            #cv2.imwrite(output,img2)
+            cv2.imwrite(output3,img2)
         
         
         
